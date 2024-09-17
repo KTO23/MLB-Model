2b36fcd8128742115d99324a522055c14c7a1ede
KTO23 <kyle.okura@yahoo.com>
2024-09-17 11:59:59 -0600

    separated data mining algos, created new model py file

--- model.py
+++ model.py
@@ -1,33 +1,30 @@
-from bs4 import BeautifulSoup
-import requests
 import pandas as pd
 
+pitcher_data = pitcher_df
+#create func in pitcher_data.py
+team_data = team_runs_per_game_df
+#create func in team_data.py
+
+home_team = "Arizona"
+away_team = "Boston"
+home_team_pitcher = "Chris Sale"
+away_team_pitcher = ""
 
 
-#url2 = 'https://razzball.com/mlbpitchingstats/'
-#page2 = requests.get(url2)
-#soup2 = BeautifulSoup(page2.text, features="html.parser")
+home_team_rpg = 0
+home_team_rpg_last3 = 0
+home_team_rpg_at_home = 0
+away_team_rpg = 0
+away_team_rpg_last3 = 0
+away_team_rpg_while_away = 0
 
-
-url2 = 'https://www.fangraphs.com/leaders/major-league?pos=all&stats=pit&type=8&pageitems=2000000000&qual=0&pagenum=1'
-page2 = requests.get(url2)
-soup2 = BeautifulSoup(page2.text, features='html.parser')
-
-pitcher_table = soup2.find_all('table')[8]
-pitcher_table_labels = pitcher_table.find('tr')
-pitcher_table_labels_new = pitcher_table_labels.find_all('th')
-pitcher_table_labels_final = [title.text for title in pitcher_table_labels_new]
-
-
-pitcher_df = pd.DataFrame(columns=pitcher_table_labels_final)
-
-
-pitcher_col_data = pitcher_table.find_all('tr')
-for row in pitcher_col_data[1:]:
-    row_data = row.find_all('td')
-    individual_row_data = [data.text for data in row_data]
-
-    length = len(pitcher_df)
-    pitcher_df.loc[length] = individual_row_data
-
-print(pitcher_df)
+for row in team_data:
+    if team_data[Team][row] == home_team:
+        home_team_rpg = team_data['2024'][row]
+        home_team_rpg_last3 = team_data['Last 3'][row]
+        home_team_rpg_at_home = team_data['Home'][row]
+    elif team_data[Team][row] == away_team:
+        away_team_rpg = team_data['2024'][row]
+        away_team_rpg_last3 = team_data['Last 3'][row]
+        away_team_rpg_while_away = team_data['Away'][row]
+    
---
+++ pitcher_data.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+
+#url2 = 'https://razzball.com/mlbpitchingstats/'
+#page2 = requests.get(url2)
+#soup2 = BeautifulSoup(page2.text, features="html.parser")
+
+
+url2 = 'https://www.fangraphs.com/leaders/major-league?pos=all&stats=pit&type=8&pageitems=2000000000&qual=0&pagenum=1'
+page2 = requests.get(url2)
+soup2 = BeautifulSoup(page2.text, features='html.parser')
+
+pitcher_table = soup2.find_all('table')[8]
+pitcher_table_labels = pitcher_table.find('tr')
+pitcher_table_labels_new = pitcher_table_labels.find_all('th')
+pitcher_table_labels_final = [title.text for title in pitcher_table_labels_new]
+
+
+pitcher_df = pd.DataFrame(columns=pitcher_table_labels_final)
+
+
+pitcher_col_data = pitcher_table.find_all('tr')
+for row in pitcher_col_data[1:]:
+    row_data = row.find_all('td')
+    individual_row_data = [data.text for data in row_data]
+    length = len(pitcher_df)
+    pitcher_df.loc[length] = individual_row_data
+
+print(pitcher_df)

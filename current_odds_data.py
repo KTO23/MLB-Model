d8271d7c0fb31e88a03ad592318ad96425e4c50b
KTO23 <kyle.okura@yahoo.com>
2024-09-25 21:53:47 -0600

    finished getting current odds

--- current_odds_data.py
+++ current_odds_data.py
@@ -15,11 +15,13 @@ def get_current_odds_data_func():
 
     #sorts html to find the table titles - used to put into our dataframe
     games_table = soup.find_all('table')[0]
+    '''
     #print(games_table)
     temp = games_table.find('tr')
     labels = temp.find_all('th')
     column_titles = [title.text for title in labels]
-    column_titles[0] = "Team"
+    '''
+    column_titles = ["Team", "Run Line", "Run Line Juice", "Total", "Total Juice", "Moneyline"]
     oddsdf = pd.DataFrame(columns=column_titles)
 
     #print(column_titles)
@@ -32,37 +34,34 @@ def get_current_odds_data_func():
         team_temp2 = team_temp.find_all('div')[7]
         team_name = [data.text for data in team_temp2]
 
-        row_data = 
+        #run line
+        run_line_data = row.find_all('div')[13]
+        run_line = [data.text for data in run_line_data]
+
+        #run line juice
+        run_line_juice_data = row.find_all('div')[16]
+        run_line_juice = [data.text for data in run_line_juice_data]
+
+        #total
+        total_data = row.find_all('div')[20]
+        total_line_temp = [data.text for data in total_data]
+        total_line = [total_line_temp[0] + total_line_temp[2]]
+
+        #total_juice
+        total_juice_data = row.find_all('div')[19]
+        total_line_juice_temp = [data.text for data in total_juice_data]
+        total_line_juice = [total_line_juice_temp[1]]
+
+        #ml
+        ml_data = row.find_all('div')[24]
+        ml = [data.text for data in ml_data]
+        
+        row_data = [team_name[0], run_line[0], run_line_juice[0], total_line[0], total_line_juice[0], ml[0]]
         length = len(oddsdf)
-        #oddsdf.loc[length] = row_text
+        oddsdf.loc[length] = row_data
     
 
     print(oddsdf)
-
-    '''
-    #getting all rows of the table
-    pitcher_col_data = games_table.find_all('tr')
-    #inserting each row data into our dataframe
-    for row in pitcher_col_data[1:]:
-        row_data = row.find_all('td')
-        row_text = [data.text for data in row_data]
-        length = len(pitcherdf)
-        pitcherdf.loc[length] = row_text
-
-    #print(pitcherdf)
-    return pitcherdf
-
-    #troubleshooting
-    #print(pitcherdf)
-
-def print_unqiue_pitcher_data_teams():
-    pitcherdf = pitcher_data_func()
-    print("Pitcher Data Unique Teams")
-    print(pitcherdf['Team'].unique())
-
-pitcher_data_func()
-#print_unique_pitcher_data_teams()
-'''
 
 
 get_current_odds_data_func()
